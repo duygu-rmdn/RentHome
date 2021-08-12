90b37588dbb71b35a8f95c5782b5af92e6dde770
Duygu <duygumetin99@gmail.com>
2021-08-12 14:40:32 +0300

    added administration role

---
+++ RentHome/Web/RentHome.Web/Areas/Administration/Controllers/PropertiesController.cs
@@ -0,0 +1,12 @@
+﻿namespace RentHome.Web.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PropertiesController : AdministrationController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
