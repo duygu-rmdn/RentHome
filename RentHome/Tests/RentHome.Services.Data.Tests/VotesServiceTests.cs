491a30e6c630a778e5e4c4852bed96382a03f021
Duygu <duygumetin99@gmail.com>
2021-08-13 13:15:03 +0300

    tested votes service

---
+++ RentHome/Tests/RentHome.Services.Data.Tests/VotesServiceTests.cs
@@ -0,0 +1,62 @@
+﻿namespace RentHome.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Moq;
+    using RentHome.Data.Common.Repositories;
+    using RentHome.Data.Models;
+    using Xunit;
+
+    public class VotesServiceTests
+    {
+        [Fact]
+        public async Task WhenUserVotes2TimesOnly1ShouldByCounted()
+        {
+            var list = new List<Vote>();
+            var mockRepo = new Mock<IRepository<Vote>>();
+            mockRepo.Setup(x => x.All()).Returns(list.AsQueryable());
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => list.Add(vote));
+            var service = new VotesService(mockRepo.Object);
+
+            await service.SetVoteAsync("1", "2", 1);
+            await service.SetVoteAsync("1", "2", 5);
+            await service.SetVoteAsync("1", "2", 5);
+            await service.SetVoteAsync("1", "2", 5);
+            await service.SetVoteAsync("1", "2", 5);
+
+            Assert.Equal(1, list.Count);
+            Assert.Equal(5, list.First().Value);
+        }
+
+        [Fact]
+        public async Task When2UsersVoteFor1PropertyAverageVoteShouldBeCorrect()
+        {
+            var list = new List<Vote>();
+            var mockRepo = new Mock<IRepository<Vote>>();
+            mockRepo.Setup(x => x.All()).Returns(list.AsQueryable());
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => list.Add(vote));
+            var service = new VotesService(mockRepo.Object);
+
+            await service.SetVoteAsync("1", "1", 1);
+            await service.SetVoteAsync("1", "2", 5);
+            await service.SetVoteAsync("1", "2", 2);
+
+            Assert.Equal(2, list.Count);
+            Assert.Equal(1.5, service.GetAverageVote("1"));
+        }
+
+        [Fact]
+        public async Task WhenVotestCountAre0AverageVoteShouldBe0()
+        {
+            var list = new List<Vote>();
+            var mockRepo = new Mock<IRepository<Vote>>();
+            mockRepo.Setup(x => x.All()).Returns(list.AsQueryable());
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Vote>())).Callback((Vote vote) => list.Add(vote));
+            var service = new VotesService(mockRepo.Object);
+
+            Assert.Equal(0, list.Count);
+        }
+    }
+}
