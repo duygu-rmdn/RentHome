233c58e4a1c7d7c066e11569d3ab1cf26db64318
Duygu <duygumetin99@gmail.com>
2021-08-14 17:35:10 +0300

    tested administration area

---
+++ RentHome/Tests/RentHome.Web.Tests/AdministrationControllerTests.cs
@@ -0,0 +1,28 @@
+﻿namespace RentHome.Web.Tests
+{
+    using MyTested.AspNetCore.Mvc;
+    using RentHome.Web.Areas.Administration.Controllers;
+    using RentHome.Web.ViewModels.Properties;
+    using Xunit;
+
+    using static RentHome.Web.Tests.Data.PropertiesData;
+
+    public class AdministrationControllerTests
+    {
+        [Fact]
+        public void IndexShouldReturnCorrectViewModel()
+            => MyController<PropertiesController>
+                .Instance()
+            .Calling(x => x.Index())
+            .ShouldReturn()
+            .View(view => view
+                .WithModelOfType<PropertiesListViewModel>());
+
+        [Fact]
+        public void ChangeVisibilityShouldReturnCorrectViewModel()
+            => MyController<PropertiesController>
+                .Instance(contoller => contoller.WithData(Property()))
+            .Calling(x => x.ChangeVisibility("1"))
+            .ShouldReturn();
+    }
+}
